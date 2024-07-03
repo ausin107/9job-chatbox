0a1d1b4fd45d40e2f7e555a4cc238f7e9a7b5d3a
ausin107 <nguyenphuqui396@gmail.com>
2024-07-03 16:23:40 +0700

    fix ui, add login btn

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import logo from '../assest/logo.png'
-
 function Navbar() {
+  const navigate = useNavigate()
+
   return (
     <div className='navbar'>
-      <img src={logo} alt='Logo' />
+      <img className='navbar-logo' src={logo} alt='Logo' />
+      <div className='navbar-login-btn' onClick={() => navigate('/login')}>
+        Login
+      </div>
     </div>
   )
 }
